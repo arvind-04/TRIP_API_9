b44e83aa4f0d288e8307cdfca788e8389ba8c58f
arvind-04 <chouhanarvindudpr@gmail.com>
2025-09-27 16:24:00 +0530

    up

--- index.js
+++ index.js
@@ -164,6 +164,9 @@ app.post("/itinerary", async (req, res) => {
   const itinerary = await generateItinerary(city, startDate, endDate, budget, currency);
   res.render("result", { itinerary: JSON.stringify(itinerary, null, 2) });
 });
+
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
